ac23f604750bda0735510e318dc3c2f9abbac088
TheBaileyBrew <matthew.bailey@hthackney.com>
2018-02-12 19:07:24 -0500

    Fragment One completed with page transition. Fragment Two completed with page transition (still needs score update). Fragment Three currently working on.

--- app/src/main/java/com/thebaileybrew/firefly/quizversionone/StartQuizActivity.java
+++ app/src/main/java/com/thebaileybrew/firefly/quizversionone/StartQuizActivity.java
@@ -1,5 +1,6 @@
 package com.thebaileybrew.firefly.quizversionone;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -7,9 +8,13 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
@@ -27,7 +32,12 @@ public class StartQuizActivity extends FragmentActivity {
 
     private android.support.v4.view.PagerAdapter mPagerAdapter;
 
-    int currentScore;
+    int currentScore = 0;
+    Button questionOneSubmit;
+    Button questionTwoSubmit;
+    Button questionThreeSubmit;
+    Button questionFourSubmit;
+    ProgressBar questionProgress;
 
     //Question One Radio Buttons
     RadioGroup fragmentQuestionOne;
@@ -35,6 +45,8 @@ public class StartQuizActivity extends FragmentActivity {
     RadioButton questionOneAnswerTwo;
     RadioButton questionOneAnswerThree;
     RadioButton questionOneAnswerFour;
+
+    //Question Two Edit Text Field
     EditText editTextQuestionTwo;
 
 
@@ -60,6 +72,25 @@ public class StartQuizActivity extends FragmentActivity {
         pager.setAdapter(this.mPagerAdapter);
     }
 
+    public class ProgressBarAnimation extends Animation {
+        private ProgressBar progressBar;
+        private float from;
+        private float to;
+
+        public ProgressBarAnimation(ProgressBar progressBar, float from, float to) {
+            super();
+            this.progressBar = progressBar;
+            this.from = from;
+            this.to = to;
+        }
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            super.applyTransformation(interpolatedTime, t);
+            float value = from + (to - from) * interpolatedTime;
+            progressBar.setProgress((int) value);
+        }
+    }
+
     public void onClickSubmitQuestionOne(View view) {
         fragmentQuestionOne = findViewById(R.id.questionOneRadioGroup);
         questionOneAnswerOne = findViewById(R.id.question_one_a1);
@@ -72,30 +103,37 @@ public class StartQuizActivity extends FragmentActivity {
                 //Correct
                 Toast.makeText(getApplicationContext(), "Correct. Malcolm Reynolds is the captain.", Toast.LENGTH_LONG).show();
                 currentScore += 1;
-                displayCurrentScore(currentScore);
-                Log.i(String.valueOf(currentScore), "Current Score is: ");
                 break;
             case R.id.question_one_a2:
                 //Incorrect
-                Toast.makeText(getApplicationContext(), "River is the special stowaway.", Toast.LENGTH_LONG).show();
-                Log.i(String.valueOf(currentScore), "Current Score is: ");
+                Toast.makeText(getApplicationContext(), "Incorrect. River Tam is a special stowaway.", Toast.LENGTH_LONG).show();
                 break;
             case R.id.question_one_a3:
                 //Incorrect
-                Toast.makeText(getApplicationContext(), "Jayne is a hired gun/mercenary.", Toast.LENGTH_LONG).show();
-                Log.i(String.valueOf(currentScore), "Current Score is: ");
+                Toast.makeText(getApplicationContext(), "Incorrect. Jayne Cobb is a hired gun/mercenary.", Toast.LENGTH_LONG).show();
                 break;
             case R.id.question_one_a4:
                 //Incorrect
-                Toast.makeText(getApplicationContext(), "Zoe is the First Mate.", Toast.LENGTH_LONG).show();
-                Log.i(String.valueOf(currentScore), "Current Score is: ");
+                Toast.makeText(getApplicationContext(), "Incorrect. Zoe Washborne is the first mate.", Toast.LENGTH_LONG).show();
                 break;
             default:
                 //No Selection
                 Toast.makeText(getApplicationContext(), "Please make a selection", Toast.LENGTH_LONG).show();
-                Log.i(String.valueOf(currentScore), "Current Score is: ");
                 break;
         }
+        //Update the Score Display
+        displayCurrentScore(currentScore);
+        //Fade the Submit Button
+        questionOneSubmit = findViewById(R.id.submit_answer_question_one);
+        questionOneSubmit.setClickable(false);
+        questionOneSubmit.setText("Answer Submitted");
+        questionOneSubmit.setTextColor(getResources().getColor(R.color.grayFadeD));
+        //Animate the Progress Bar
+        questionProgress = findViewById(R.id.progressBar);
+        ProgressBarAnimation anim = new ProgressBarAnimation(questionProgress, 0, 600);
+        anim.setDuration(3000);
+        questionProgress.startAnimation(anim);
+        //Auto-advance the Page Viewer
         final ViewPager pager = (ViewPager)super.findViewById(R.id.viewPager);
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -106,6 +144,7 @@ public class StartQuizActivity extends FragmentActivity {
         }, 3000);
 
     }
+
 
     public void onClickSubmitQuestionTwo (View view) {
         editTextQuestionTwo = findViewById(R.id.question_two_answerEditText);
@@ -130,7 +169,22 @@ public class StartQuizActivity extends FragmentActivity {
                     currentScore += 1;
                     Toast.makeText(getApplicationContext(), "Correct, " + questionTwoAnswer + " is the classification", Toast.LENGTH_LONG).show();
                     break;
+                case "Fire fly":
+                    //Correct
+                    currentScore += 1;
+                    Toast.makeText(getApplicationContext(), "Correct, " + questionTwoAnswer + " is the classification", Toast.LENGTH_LONG).show();
+                    break;
                 case "FIRE FLY":
+                    //Correct
+                    currentScore += 1;
+                    Toast.makeText(getApplicationContext(), "Correct, " + questionTwoAnswer + " is the classification", Toast.LENGTH_LONG).show();
+                    break;
+                case "fire fly":
+                    //Correct
+                    currentScore += 1;
+                    Toast.makeText(getApplicationContext(), "Correct, " + questionTwoAnswer + " is the classification", Toast.LENGTH_LONG).show();
+                    break;
+                case "firefly":
                     //Correct
                     currentScore += 1;
                     Toast.makeText(getApplicationContext(), "Correct, " + questionTwoAnswer + " is the classification", Toast.LENGTH_LONG).show();
@@ -144,6 +198,17 @@ public class StartQuizActivity extends FragmentActivity {
                     break;
             }
         }
+
+        questionTwoSubmit = findViewById(R.id.submit_answer_question_two);
+        questionTwoSubmit.setClickable(false);
+        questionTwoSubmit.setText("Answer Submitted");
+        questionTwoSubmit.setTextColor(getResources().getColor(R.color.grayFadeD));
+
+        questionProgress = findViewById(R.id.progressBar);
+        ProgressBarAnimation anim = new ProgressBarAnimation(questionProgress, 600, 1200);
+        anim.setDuration(3000);
+        questionProgress.startAnimation(anim);
+
         final ViewPager pager = (ViewPager)super.findViewById(R.id.viewPager);
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -156,8 +221,10 @@ public class StartQuizActivity extends FragmentActivity {
 
 
 
+
     public void displayCurrentScore (int score) {
         TextView currentScoreTV = findViewById(R.id.currentScoreDisplay);
+        score = score * 10;
         currentScoreTV.setText(String.valueOf(score));
     }
 }
